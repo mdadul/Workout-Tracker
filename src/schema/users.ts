4bc33d2eaae7c75e580958deb330c4ccb0df741f
Emdadul Islam <emdadulislam580@gmail.com>
2024-08-31 14:48:49 +0600

    refactor: Update users table schema to remove unnecessary default values

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -1,11 +1,11 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
-  created_at: timestamp("created_at").notNull().defaultNow(),
+  created_at: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at")
     .notNull()
     .$onUpdate(() => new Date()),
